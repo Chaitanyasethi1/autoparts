c289cb01538b3833c6db53d341e46576ef8ec2a7
chaitanya sethi <chaitanyasethi3336@gmail.com>
2026-08-21 21:07:45 +0530

    style: make logo wider and scale properly

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const GBP_URL = "https://maps.google.com/?q=336+Hilton+Drive,+Stoney+Creek,+ON+L
 const BrandLogo = ({ className = "" }) => {
   return (
     <div className={`flex items-center justify-center ${className}`}>
-      <img src="/PRIMETECH_LOGO.png" alt="Primetech Auto & Tires Logo" className="h-16 md:h-20 w-auto object-contain" />
+      <img src="/PRIMETECH_LOGO.png" alt="Primetech Auto & Tires Logo" className="w-[160px] sm:w-[200px] md:w-[240px] h-auto object-contain drop-shadow-lg" />
     </div>
   )
 }
